ce525fa2942697d773f53089c0bfc8dabc7118cc
Fajriatun Munawaroh <fajriatun072@gmail.com>
2023-03-14 13:30:48 +0700

    create file Procfile

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import ProductRoute from "./routes/ProductRoute.js";
+const port = process.env.PORT||5000;
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/eduwork", {
