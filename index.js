7b783aaf1b977b11a0b33188b38a4ba8d6b03d52
Fajriatun Munawaroh <fajriatun072@gmail.com>
2023-03-14 14:41:32 +0700

    modified file index.js and package.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import ProductRoute from "./routes/ProductRoute.js";
-const PORT = process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/eduwork", {
